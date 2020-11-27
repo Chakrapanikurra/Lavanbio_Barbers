48bb8144be9356b1af2c250826056d7c108500fd
Chakrapanikurra <chakra.kurra@gmail.com>
2020-11-26 17:22:04 -0700

    Added Payment Form and pricing list

--- js/main.js
+++ js/main.js
@@ -20,5 +20,31 @@ $(document).ready(function(){
         autoplayTimeout: 3000
         
     });
+
+
+    //clock to scroll up
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
 });
 
+function openPrice() {
+    window.open("pricing.html");
+  }
+
+function openBooking(){
+    window.open("bookonline.html");
+}
+
+
+function runClock(){
+    /* Store the current date and time */
+    var currentDay = new Date();
+    var dateStr = currentDay.toLocaleDateString();
+    var timeStr = currentDay.toLocaleTimetring();
+
+    /* Display the current date and time */
+    window.document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+}
